ec4bf7a199c5743b181ca9addd2b886b3983ff0c
Your Name <you@example.com>
2024-08-26 13:48:31 +0700

    baik

--- bu pasha/Guru.java
+++ bu pasha/Guru.java
@@ -3,23 +3,26 @@ public class Guru {
     String nama, mapel, status;
 
 
-public Guru (int p, String n, String m, String s){
-  nip = p;
-  nama = n;
-  mapel = m;
-  status = s;
+  public Guru (int p, String n, String m, String s){
+    nip = p;
+    nama = n;
+    mapel = m;
+    status = s;
+  }
 
+  public void print() {
   System.out.println("Nip : "+nip);
-  System.out.println("---------------------");
+
   System.out.println("Nama : "+nama);
-  System.out.println("---------------------");
+
   System.out.println("Mapel : "+mapel);
-  System.out.println("---------------------");
+
   System.out.println("Status : "+status);
-  System.out.println("---------------------");
+
   System.out.println("");
+  }
 
  
 }
 
-}
+
--- bu pasha/GuruTester.java
+++ bu pasha/GuruTester.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class GuruTester {
     public static void main(String[] args) {
-        Guru buPasha = new Guru(3021, "Bu pasha", "Produktif rpl", "tetap");
-        Guru pakTulus = new Guru(3022, "Pak tulus", "Ipas", "part time");
-        Guru pakFalah = new Guru(3023, "Bu pasha", "Bahasa indonesia", "tetap");
-        Guru buAul = new Guru(3024, "Bu pasha", "Produktif rpl", "part-time");
-        Guru buJevi = new Guru(3025, "Bu pasha", "Bahasa inggris", "tetap");
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.println("masukkan nama : ");
+        String nama = input.nextLine();
+        
+
+        System.out.println("masukkan mapel : ");
+        String mapel = input.nextLine();
+
+        System.out.println("masukkan status : ");
+        String status = input.nextLine();
+
+        System.out.println("masukkan nip : ");
+        int nip = input.nextInt();
+
+        
+        System.out.println("-------------------");
+
+        input.close();
+
+        Guru guru = new Guru(nip, nama, mapel, status);
+        guru.print();
+        // Guru buPasha = new Guru(3021, "Bu pasha", "Produktif rpl", "tetap");
+        // Guru pakTulus = new Guru(3022, "Pak tulus", "Ipas", "part time");
+        // Guru pakFalah = new Guru(3023, "Bu pasha", "Bahasa indonesia", "tetap");
+        // Guru buAul = new Guru(3024, "Bu pasha", "Produktif rpl", "part-time");
+        // Guru buJevi = new Guru(3025, "Bu pasha", "Bahasa inggris", "tetap");
         
     }
 }
